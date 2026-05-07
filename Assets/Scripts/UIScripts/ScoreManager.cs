df5512ef38d7001dfd834d841201c341c1ed485c
MadnessAdvocate <agentlazarus623@gmail.com>
2026-05-07 07:24:14 +0100

    Remnants of my project crashing: some of End result screen
    i hate unity

---
+++ Assets/Scripts/UIScripts/ScoreManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using TMPro;
+public class ScoreManager : MonoBehaviour
+{
+    public TMP_Text scoreText;
+    [SerializeField] private int scoreAmount = 0;
+
+    public void AddScore()
+    {
+        scoreAmount++;
+        scoreText.text = "Score: " + scoreAmount.ToString();
+    }
+}
