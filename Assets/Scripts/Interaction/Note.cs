cd093d7c11d240e952e9159406fc3d44f671ebda
Smudgy0 <theohammond2006@gmail.com>
2026-05-07 10:51:34 +0100

    comments

--- Assets/Scripts/Interaction/Note.cs
+++ Assets/Scripts/Interaction/Note.cs
@@ -12,6 +12,7 @@ public class Note : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // if the player walks into the note send a string to the note manager to change the ui
         if(collision.tag == "Player")
         {
             NM.ChangeNoteDesc(StoryMessage);
--- Assets/Scripts/Interaction/NoteManager.cs
+++ Assets/Scripts/Interaction/NoteManager.cs
@@ -5,6 +5,7 @@ public class NoteManager : MonoBehaviour
 {
     public TMP_Text NoteDesc;
 
+    // change ui text to the string value of the note
     public void ChangeNoteDesc(string text)
     {
         NoteDesc.text = text;
